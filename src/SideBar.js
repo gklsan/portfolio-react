ed784cd57af8ddb1b34081534f918e2c6d87b3fa
Gokul <pgokulmca@gmail.com>
2019-12-22 15:01:23 +0530

    basic page template added with sidebar

---
+++ src/SideBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SideBar = () => {
+  return(
+    <div className="col-md-4">
+      <div>
+        <h1>Gokul P</h1>
+      </div>
+    </div>
+  )
+};
+
+export default SideBar;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import React from "react";
 import { render } from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SideBar from "./SideBar";
+
+const Page = () => (
+  <div className="row">
+    <SideBar/>
+    <h2 className="col-md-7">
+      Other text Other text Other text Other text Other text Other text Other text Other
+      Other text Other text Other text Other text Other text Other text Other text Other
+      Other text Other text Other text Other text Other text Other text Other text Other
+      Other text Other text Other text Other text Other text Other text Other text Other
+      Other text Other text Other text Other text Other text Other text Other text Other
+      Other text Other text Other text Other text Other text Other text Other text Other
+      Other text Other text Other text Other text Other text Other text Other text Other
+      Other text Other text Other text Other text Other text Other text Other text Other
+      Other text Other text Other text Other text Other text Other text Other text Other
+      Other text Other text Other text Other text Other text Other text Other text Other
+      Other text Other text Other text Other text Other text Other text Other text Other
+      text Other text Other text Other text Other text Other text
+    </h2>
+  </div>
+);
 
 render(
-  <h1>Gokul</h1>,
+  <Page />,
   document.getElementById('root')
 );
